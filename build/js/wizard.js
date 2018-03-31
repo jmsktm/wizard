f9d34c699738585a7e8cb919ebbd22ae5d9efd87
James <james@icloudtech.com>
2018-03-31 01:07:30 -0700

    Removing build/ folder from .gitignore

---
+++ build/js/wizard.js
@@ -0,0 +1,6 @@
+(function(global) {
+	console.log('Inside file1');
+})(typeof window !== 'undefined' ? window : this, undefined);;
+(function(global) {
+	console.log('Inside file2');
+})(typeof window !== 'undefined' ? window : this, undefined);
