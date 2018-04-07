71e6475f9e2e436ef44bc649bebcf32912692141
James <james@icloudtech.com>
2018-04-07 13:59:02 -0700

    Removing jquery from requirejs

--- src/script/config.js
+++ src/script/config.js
@@ -1,10 +1,9 @@
 (function(global) {
 
   'use strict';
-  
+
   requirejs.config({
     paths: {
-      jquery: 'https://code.jquery.com/jquery-3.3.1.min.js'
     }
   });
 })(typeof window !== 'undefined' ? window : this);
