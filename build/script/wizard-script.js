1dd9ef76ca661cc9e16276941e2298628deabf61
James <james@icloudtech.com>
2018-04-07 11:33:19 -0700

    WIP: Loading Google Analytics script based on config from page

--- build/script/wizard-script.js
+++ build/script/wizard-script.js
@@ -30,8 +30,35 @@
 (function(global, undefined) {
 
   'use strict';
-  
-	console.log('Inside file2!');
+
+  /* Google Analytics setup using site config obtained from page */
+
+  var module;
+
+  // Setup temporary Google Analytics objects.
+  global.GoogleAnalyticsObject = "ga";
+  global.ga = function () { (global.ga.q = global.ga.q || []).push(arguments); };
+  global.ga.l = 1 * new Date();
+
+  if (typeof window.define === "function" && window.define.amd && typeof window.require === "function") {
+    require(['siteConfig'], function(siteConfig) {
+      global.ga("create", siteConfig.googleAnalytics, siteConfig.domain);
+      global.ga("send", "pageview");
+    });
+  }
+
+  /*
+  require(["ga", "siteConfig"], function(ga, siteConfig) {
+    if (siteConfig && siteConfig.googleAnalytics) {
+      global.GoogleAnalyticsObject = "__ga__";
+      global.__ga__ = {
+          q: [["create", siteConfig.googleAnalytics, "auto"]],
+          l: Date.now()
+      };
+      ga("send", "pageview");
+    }
+  });*/
+
 })(typeof window !== 'undefined' ? window : this);
 (function(global, undefined) {
 
